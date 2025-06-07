09f44ea868ab57811641a77c3958c406c0a6c472
biosunhongxun <bio-sunhongxun@outlook.com>
2025-06-07 17:00:44 +0800

    Deployed 9600b77 with MkDocs version: 1.6.1

--- js/extra_javascript.js
+++
@@ -1,8 +0,0 @@
-// 自动更新年份
-document.addEventListener("DOMContentLoaded", () => {
-  const yearSpan = document.getElementById("year");
-  if (yearSpan) {
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear > 2025 ? `2025-${currentYear}` : "2025";
-  }
-});
